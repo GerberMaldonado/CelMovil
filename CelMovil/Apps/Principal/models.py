1c162ca8beee350020c666ee50ab2c21e2a8efcf
Elias Mérida <eliasm_84@hotmail.com>
2019-10-05 01:15:31 -0600

    Coneccion de BD Mysql

--- CelMovil/Apps/Principal/models.py
+++ CelMovil/Apps/Principal/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Accesorios(models.Model):
+    idAccesorios = models.CharField(max_length=50)
+    codigo = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=200)
+    marca = models.DateTimeField(auto_now_add=True)
+    fechaingreso = models.DateTimeField(auto_now_add=True)
+    cantidad = models.FloatField()
+
+    
--- CelMovil/CelMovil/settings.py
+++ CelMovil/CelMovil/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'CelMovil.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
