7bc09b865fdf647ca5599a6063ffe8ca04805bea
Gerber Maldonado <g.maldonadof25@gmail.com>
2019-10-05 14:15:45 -0600

    Modelos base de datos
    - Los modelos estaban en principal y fueron cambiados a administracion

--- CelMovil/Apps/Administracion/admin.py
+++ CelMovil/Apps/Administracion/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Accesorios,Celulares,Chips,Cliente,Detalle_Venta,Reparaciones,Repuestos,Usuario,Vendedor,Ventas
 # Register your models here.
+
+admin.site.register(Accesorios)
+admin.site.register(Celulares)
+admin.site.register(Chips)
+admin.site.register(Cliente)
+admin.site.register(Detalle_Venta)
+admin.site.register(Reparaciones)
+admin.site.register(Repuestos)
+admin.site.register(Usuario)
+admin.site.register(Vendedor)
+admin.site.register(Ventas)
--- CelMovil/Apps/Principal/admin.py
+++ CelMovil/Apps/Principal/admin.py
@@ -1,13 +1,3 @@
 from django.contrib import admin
-from .models import Accesorios,Celulares,Chips,Cliente,Detalle_Venta,Reparaciones,Repuestos,Usuario,Vendedor,Ventas
 
-admin.site.register(Accesorios)
-admin.site.register(Celulares)
-admin.site.register(Chips)
-admin.site.register(Cliente)
-admin.site.register(Detalle_Venta)
-admin.site.register(Reparaciones)
-admin.site.register(Repuestos)
-admin.site.register(Usuario)
-admin.site.register(Vendedor)
-admin.site.register(Ventas)
+# Register your models here.
