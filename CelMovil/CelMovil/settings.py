833d37febc2e019f022bfe5476da238996df7917
Elias Mérida <eliasm_84@hotmail.com>
2019-10-05 13:48:45 -0600

    Modelos de la base de datos

--- CelMovil/CelMovil/settings.py
+++ CelMovil/CelMovil/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mydb',
+        'NAME': 'celmovil',
         'USER': 'root',
         'PASSWORD': 'root',
         'HOST': 'localhost',
