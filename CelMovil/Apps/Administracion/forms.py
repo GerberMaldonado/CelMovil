bb4fec56dfe6d599a7a8dce89b2fd571dec95999
Gerber Maldonado <g.maldonadof25@gmail.com>
2019-10-19 16:58:35 -0600

    Arreglo del modulo repuestos

--- CelMovil/Apps/Administracion/forms.py
+++ CelMovil/Apps/Administracion/forms.py
@@ -29,20 +29,20 @@ class ReparacionesForm(forms.ModelForm):
 		'descripcionfalla': forms.TextInput(attrs={'class': 'form-control', 'type': 'text'}),
 		'fechasalida': forms.TextInput(attrs={'class': 'form-control', 'type': 'date'}),
 		'costo': forms.TextInput(attrs={'class': 'form-control', 'type': 'number'}),
-		'clientes': forms.Select(attrs={'class': 'form-control fill'}),		
-		}								
-
+		'clientes': forms.Select(attrs={'class': 'form-control fill'}),
+		}
+		
 class RepuestosForm(forms.ModelForm):
 	class Meta:
 		model = Repuestos
 		fields = '__all__'
 		widgets = {
-		'codifo'
-		'nombre'
-		'marca'
-		'modelo'
-		'cantidad'
-		'preciomayor'
-		'preciounidad'
+		'codigo': forms.TextInput(attrs={'class': 'form-control', 'type': 'text'}),
+		'nombre': forms.TextInput(attrs={'class': 'form-control', 'type': 'text'}),
+		'marca': forms.TextInput(attrs={'class': 'form-control', 'type': 'text'}),
+		'modelo': forms.TextInput(attrs={'class': 'form-control', 'type': 'text'}),
+		'cantidad': forms.TextInput(attrs={'class': 'form-control', 'type': 'number'}),
+		'preciomayor': forms.TextInput(attrs={'class': 'form-control', 'type': 'number'}),
+		'preciounidad': forms.TextInput(attrs={'class': 'form-control', 'type': 'number'}),
 		'reparaciones': forms.Select(attrs={'class': 'forms-control fill'}),
 		}
