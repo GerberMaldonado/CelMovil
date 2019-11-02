4007e0639efb77a79ad2fd69b97e1b008257af42
RonyFL <ronysi10@gmail.com>
2019-11-02 03:16:36 -0600

    Principal
    Diseño

--- CelMovil/CelMovil/settings.py
+++ CelMovil/CelMovil/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'celmovil',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
